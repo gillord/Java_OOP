410078ae800fa054fb82dee6db32f3732f793758
Gillory, Douglas "DJ <gillord@stthom.edu>
2019-07-14 04:17:00 -0500

    Add files via upload

---
+++ WorldSeriesChampions/src/worldserieschampions/WSWinners.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldserieschampions;
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class WSWinners{
+
+    private static String stringList;
+/**
+ * @param argv 
+ * @throws java.lang.Exception 
+ *
+ * 
+ * @author DJ
+ */
+   
+    public static void main(String[] argv)throws Exception{
+     
+        
+   WorldSeriesChampions team = new WorldSeriesChampions();
+   File file = new File(stringList);
+   Scanner inputFile = new Scanner("WorldSeriesChampions.txt");
+   ArrayList <String> stringList = new ArrayList<>();
+   
+    stringList.add ("Boston Americans");
+    stringList.add ("New York Giants");
+    stringList.add ("Chicago White Sox");
+    stringList.add ("Chicago Cubs");
+    stringList.add ("Chicago Cubs");
+    stringList.add (" Pittsburgh Pirates");
+    stringList.add ("Philadelphia Athletics");
+    stringList.add ("Philadelphia Athletics");
+    stringList.add ("Boston Red Sox");
+    stringList.add ("Philadelphia Athletics");
+    stringList.add ("Boston Braves");
+    stringList.add ("Boston Red Sox");
+    stringList.add ("Boston Red Sox");
+    stringList.add ("Chicago White Sox");
+    stringList.add ("Boston Red Sox");
+    stringList.add ("Cincinnati Reds");
+    stringList.add ("Cleveland Indians");
+    stringList.add ("New York Giants");
+    stringList.add ("New York Giants");
+    stringList.add ("New York Yankees");
+    stringList.add ("Washington Senators");
+    stringList.add ("Pittsburgh Pirates");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("Philadelphia Athletics");
+    stringList.add ("Philadelphia Athletics");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Giants");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("Detroit Tigers");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("Cincinnati Reds");
+    stringList.add ("New York Yankees");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("New York Yankees");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("Detroit Tigers");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("New York Yankees");
+    stringList.add ("Cleveland Indians");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Giants");
+    stringList.add ("Brooklyn Dodgers");
+    stringList.add ("New York Yankees");
+    stringList.add ("Milwaukee Braves");
+    stringList.add ("New York Yankees");
+    stringList.add ("Los Angeles Dodgers");
+    stringList.add ("Pittsburgh Pirates");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("Los Angeles Dodgers");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("Los Angeles Dodgers");
+    stringList.add ("Baltimore Orioles");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("Detroit Tigers");
+    stringList.add ("New York Mets");
+    stringList.add ("Baltimore Orioles");
+    stringList.add ("Pittsburgh Pirates");
+    stringList.add ("Oakland Athletics");
+    stringList.add ("Oakland Athletics");
+    stringList.add ("Oakland Athletics");
+    stringList.add ("Cincinnati Reds");
+    stringList.add ("Cincinnati Reds");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("Pittsburgh Pirates");
+    stringList.add ("Philadelphia Phillies");
+    stringList.add ("Los Angeles Dodgers");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("Baltimore Orioles");
+    stringList.add ("Detroit Tigers");
+    stringList.add ("Kansas City Royals");
+    stringList.add ("New York Mets");
+    stringList.add ("Minnesota Twins");
+    stringList.add ("Los Angeles Dodgers");
+    stringList.add ("Oakland Athletics");
+    stringList.add ("Cincinnati Reds");
+    stringList.add ("Minnesota Twins");
+    stringList.add ("Toronto Blue Jays");
+    stringList.add ("Toronto Blue Jays");
+    stringList.add ("Atlanta Braves");
+    stringList.add ("New York Yankees");
+    stringList.add ("Florida Marlins");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("New York Yankees");
+    stringList.add ("Arizona Diamondbacks");
+    stringList.add ("Anaheim Angels");
+    stringList.add ("Florida Marlins");
+    stringList.add ("Boston Red Sox");
+    stringList.add ("Chicago White Sox");
+    stringList.add ("St. Louis Cardinals");
+    stringList.add ("Boston Red Sox");
+    stringList.add ("Philadelphia Phillies");
+
+
+    
+    }
+    
+}
